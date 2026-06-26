f41c2051bec1d389bdcb610130904e92d67e0c65
Nakshatra Namaha Creations <119853069+NakshatraNamahaCreations@users.noreply.github.com>
2026-06-26 17:50:09 +0530

    Fix CORS: allow griffinsolutions26.netlify.app

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ const ALLOWED_ORIGINS = [
   'http://localhost:3001',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:3001',
+  'https://griffinbackend.onrender.com',
+  'https://griffinsolutions26.netlify.app',
 ].filter(Boolean);
 
 app.use(cors({
